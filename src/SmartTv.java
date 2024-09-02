f2006a2eb881171780ff932195800432609156bd
wendersonaguiar <wendersonaguiar@mec.gov.br>
2024-09-02 17:26:15 -0300

    atualização de saida

--- src/SmartTv.java
+++ src/SmartTv.java
@@ -2,4 +2,17 @@ public class SmartTv{
     boolean ligada=false;
     int canal=1;
     int volume = 25;
+
+    public void aumentarVolume(){
+        volume++;
+    }
+    public void diminuirVolume(){
+        volume--;
+    }
+    public void ligar(){
+        ligada=true;
+    }
+    public void desligar(){
+        ligada=false;
+    }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,10 +1,23 @@
 public class Usuario{
 
     public static void main(String[] args) throws Exception{
-        SmartTV smartTv = new SmartTv();
+        
+        SmartTv smartTv = new SmartTv();
 
-        System.out.println("TV Ligada" + smartTv.ligada);
-        System.out.println("Canal Atual" + smartTv.canal);
-        System.out.println("Volume Atual" + smartTv.volume);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual : " + smartTv.volume);
+        
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual : " + smartTv.canal);
+        System.out.println("Volume Atual : " + smartTv.volume);
+        
+        smartTv.ligar();
+        System.out.println("Novo status -> Tv Ligada ? " + smartTv.ligada);
+       
+        smartTv.desligar();
+        System.out.println("Novo status -> Tv Ligada ? " + smartTv.ligada);
     }
 }
